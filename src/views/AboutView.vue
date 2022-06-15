95efaf6b6a2b01e3049a8d6c8f6655a0d5883671
rahulatlamp <rahulrajan@lampros.tech>
2022-06-15 18:06:00 +0530

    Latest Code

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -20,7 +20,7 @@ export default {
   },
   methods: {
     fetchTransactions() {
-      axios.get('http://173.82.54.146:5000/daily').then((res) => {
+      axios.get('http://127.0.0.1:5000/daily').then((res) => {
         this.info.push(res.data);
         // console.log(res.data);
       }).catch();
